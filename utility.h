d9b554ce32ccbe04ca595b7cdf840a75e4986d97
sesheii <118640498+sesheii@users.noreply.github.com>
2023-03-15 00:22:32 +0200

    Create utility.h

---
+++ utility.h
@@ -0,0 +1,11 @@
+#ifndef LABOOP_UTILITY_H
+#define LABOOP_UTILITY_H
+#include <vector>
+#include <string>
+
+extern std::vector<std::string> male_names;
+extern std::vector<std::string> female_names;
+extern std::vector<std::string> male_surnames;
+extern std::vector<std::string> female_surnames;
+
+#endif
