a0bcb64aba2cecb3cf7ba197297ad87b6108caba
sesheii <118640498+sesheii@users.noreply.github.com>
2023-03-17 02:46:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,6 +37,8 @@ public:
 
     virtual void TellWithdrawalQuery(uint64_t amount) = 0;
     virtual void TellDepositQuery(uint64_t amount) = 0;
+    virtual void TellLoanQuery(uint64_t amount, uint64_t years) = 0;
+    virtual void TellRobQuery(int lower, int higher) = 0;
 
     virtual void greetCashier() = 0;
 
@@ -101,6 +103,24 @@ public:
                      "| 2. deny the query\n"
                      "| 3. check whether ID is fake\n"
                      "| 4. check whether the person can use bank\n"
+                     "==========================================================\n";
+    }
+
+    void loan_displayOptions(){
+        std::cout << "Type in a number that corresponds to the text next to it.\n"
+                     "| 1. accept the query\n"
+                     "| 2. deny the query\n"
+                     "| 3. check whether ID is fake\n"
+                     "| 4. check whether the person can use bank\n"
+                     "| 5. check person's income\n"
+                     "==========================================================\n";
+    }
+
+    void robbery_displayOptions(){
+        std::cout << "Type in a number that corresponds to the text next to it.\n"
+                     "| 1. give all money\n"
+                     "| 2. shoot them with shotgun\n"
+                     "| 3. try to guess number\n"
                      "==========================================================\n";
     }
 
@@ -286,6 +306,158 @@ public:
                     std::cout << person->getName() << ' ' << (person->ableToUseBank() ? "can use bank\n" : "is banned from using bank\n");
             }
         }
+        else if (type == "robbery") {
+            person->greetCashier();
+            int higher = rn(0, 100);
+            int lower = rn(0, higher);
+            person->TellRobQuery(lower, higher);
+            robbery_displayOptions();
+            int j;
+            std::cin >> j;
+            if (j == 3){
+                int answer = rn(lower, higher);
+
+                int max_guesses = 0;
+                int powOf2 = 1;
+                while (powOf2 <= answer) {
+                    powOf2 <<= 1;
+                    max_guesses++;
+                }
+
+                for (int i = 0; i < max_guesses; ++i) {
+                    int t;
+                    std::cin >> t;
+                    if (t == answer) {
+                        std::cout << "You defeated the robber! (+15 points)\n";
+                        points += 15;
+                        resolveQuery();
+                        return points;
+                    }
+                    if (t > answer) {
+                        std::cout << "Too high!\n";
+                    } else {
+                        std::cout << "Too low!\n";
+                    }
+                }
+
+                std::cout << "You lost! (-50 points)\n";
+                points -= 50;
+            }
+            else if (j == 1) {
+                std::cout << "You lost! (-50 points)\n";
+                points -= 50;
+                return points;
+            }
+
+            else if (j == 2) {
+                if (rn(0,1)){
+                    std::cout << "You lost! (-50 points)\n";
+                    points -= 50;
+                    return points;
+                }
+                else {
+                    std::cout << "You got lucky! (+20 points)\n";
+                    points += 20;
+                    return points;
+                }
+
+            }
+
+        }
+
+        else if (type == "loan") {
+
+            person->greetCashier();
+
+            int amount = rn(0, 10000);
+            int years = rn(1, 30);
+            person->TellLoanQuery(amount, years);
+
+            loan_displayOptions();
+
+            while (!resolved) {
+                int t;
+                std::cin >> t;
+
+                if (t == 1) {
+                    std::string error_message = "ERROR: ";
+
+                    if (person->getIncome()*years < amount){
+                        error_message += "This person isn't able to pay off a loan; ";
+                        points -= 15;
+                    }
+                    if (person->getId().substr(5, 4) == "FAKE") {
+                        error_message += "This person's ID is fake; ";
+                        points -= 10;
+                    }
+                    if (!person->ableToUseBank()) {
+                        error_message += "This person is banned from using bank; ";
+                        points -= 10;
+                    }
+
+                    if (person->getAccountType() == "VIP" && points < 0) {
+                        points = -5; ///that's right
+                    }
+
+
+                    if (points < 0) {
+                        std::cout << error_message
+                                  << "\nYou made a mistake while dealing with client. Your points decreased by "
+                                  << points
+                                  << " points\n";
+                    } else {
+                        if (person->getAccountType() == "VIP") {
+                            std::cout << "You successfully handled the task. Good job! (+10 points)\n";
+                            points += 10;
+                        } else {
+                            std::cout << "You successfully handled the task. Good job! (+5 points)\n";
+                            points += 5;
+                        }
+                    }
+
+                    this->resolveQuery();
+                }
+
+                else if (t == 2) {
+
+                    if (person->getId().substr(5,4) != "FAKE"
+                    && person->ableToUseBank()
+                    && person->getIncome()*years >= amount)
+                    {
+                        std::cout << "This person was not guilty of anything\n";
+
+                        if (person->getAccountType() == "VIP") {
+                            std::cout << " (-30 points)";
+                            points -= 30;
+                        }
+                        else {
+                            std::cout << " (-20 points)";
+                            points -= 20;
+                        }
+                    }
+
+                    else {
+                        if (person->getAccountType() == "VIP") {
+                            std::cout << "You successfully handled the task. Good job! (+11 points)\n";
+                            points += 11;
+                        }
+                        else {
+                            std::cout << "You successfully handled the task. Good job! (+7 points)\n";
+                            points += 7;
+                        }
+                    }
+
+                    this->resolveQuery();
+                }
+
+                else if (t == 3)
+                    std::cout << person->getName() << "'s ID is " << person->getId() << '\n';
+                else if (t == 4)
+                    std::cout << person->getName() << ' ' << (person->ableToUseBank() ? "can use bank\n" : "is banned from using bank\n");
+                else if (t == 5)
+                    std::cout << person->getName() << "'s income is " << person->getIncome() << '\n';
+            }
+        }
         return points;
     }
 };
@@ -306,6 +478,15 @@ public:
         std::cout << "Hello! my name is " << this->name << '\n';
     }
 
+    void TellRobQuery(int lower, int higher) override {
+        std::cout << "HAHA, i'm a robber. Give me all your money or die or guess a number between "
+        << lower << " and " << higher << " and have a nice day\n";
+    }
+
+    void TellLoanQuery(uint64_t amount, uint64_t years) override {
+        std::cout << "I would like to take a loan in the size of " + std::to_string(amount) + " UAH for " << years << " years with a 0 interest rate.\n";
+    }
+
     void TellDepositQuery(uint64_t amount) override {
         std::cout << "I would like to deposit " + std::to_string(amount) + " UAH into my bank account.\n";
     }
@@ -319,16 +500,25 @@ class Alien : public Humanoid {
 
 public:
     Alien() : Humanoid() {
-        species = "human";
+        species = "alien";
     }
 
     Alien(std::string name_, int age_, bool canUseBank_, std::string ID_, std::string accountType_, uint64_t balance_, uint64_t income_)
             : Humanoid(name_, age_, canUseBank_, ID_, accountType_, balance_, income_) {
-        species = "human";
+        species = "alien";
     }
 
     void greetCashier() override {
-        std::cout << "$%AC0!^6:; _=U]}\\|/?., 83[X&7:; " << this->name << '\n';
+        std::cout << "H3LL0/?.,MY N4M3 1$:; " << this->name << '\n';
+    }
+
+    void TellLoanQuery(uint64_t amount, uint64_t years) {
+        std::cout << "I W4Nt% T4K3 L0An., " + std::to_string(amount) + " U4H f0R^ " << years << " Y34RS.., 0 1nterest r4te.\n";
+    }
+
+    void TellRobQuery(int lower, int higher) override {
+        std::cout << "+&$HF, B(!|B ?+!^C D_ S;& ]qd08 ->R0BB3R. G1V3 4LL Y0UR M0N3Y 0R D13 0R GU3$$ NUMB3R B3TW33N "
+                  << lower << " 4ND " << higher << " 4ND H4VE 4 N1C3 D4Y\n";
     }
 
     void TellDepositQuery(uint64_t amount) override {
@@ -347,23 +537,33 @@ constexpr int min_points = -50;
 
 
 int main() {
+
+    std::vector<Humanoid*> clients(rn(20, 120));
+
+    for (auto& i : clients) {
+        int random_number = rn(0,3) == 0;
+
+        if (random_number == 0)
+            i = new Alien();
+        else if (random_number == 1)
+            i = new Human();
+    }
+
+    std::cout << clients.size() << " clients today!\n";
     int score = 0;
+    std::vector<std::string> QUERY_TYPE ={"deposit", "withdrawal", "robbery", "loan"};
+    for (auto i : clients){
 
-    std::vector<std::string> Q_TYPE ={"deposit", "withdrawal"};
-    while (true){
-        std::cout << "\n\n\n\n\n";
-        Humanoid* entity;
+        std::cout << "\n\n\n\n";
 
-        if (rn(0,1) == 0)
-            entity = new Alien();
-        else
-            entity = new Human();
+        int random = rn(0,QUERY_TYPE.size()-1);
 
-        Query_ q(Q_TYPE[rn(0,Q_TYPE.size()-1)], entity);
+        Query_ q(QUERY_TYPE[random], i);
         score += q.handleQuery();
 
+        std::cout << "points: " << score << '\n';
         if (score < min_points) {
-            std::cout << "GAME OVER.";
+            std::cout << "GAME OVER. Thanks for playing";
             break;
         }
     }
